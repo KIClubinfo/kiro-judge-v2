54b25f901b75061b0cfe0ae58a692060b7f8d810
Austraala <16jlray74@gmail.com>
2021-03-07 16:10:47 +0100

    Change cxhome to localhost // fix leaderboard

---
+++ front/src/features/404/view.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function NotFound() {
+    return (
+        <div id='404'>
+            Page non trouvée !
+        </div>
+    )
+}
--- front/src/features/404/view.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function NotFound() {
-    return (
-        <div id='404'>
-            Page not found!
-        </div>
-    )
-}
