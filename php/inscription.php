6825631f0fea25cfbec1c8aa8a6129dd1e47ebf0
antoninparrot <antonin-parrot@hotmail.fr>
2021-04-04 01:51:34 +0200

    Update inscription.php

--- php/inscription.php
+++ php/inscription.php
@@ -340,10 +340,13 @@ if (isset($erreur)) {
   //si on doit afficher le formulaire avec un message d'erreur
 
  popup($erreur, 6000, "error");
- popup($erreur); ?>
+ popup($erreur);
+ echo $erreur; ?>
 
   <form action="" method="post" name="inscription">
-
+    <?php popup($erreur, 6000, "error");
+    popup($erreur);
+    echo $erreur; ?>
     <div id="equipe" style="display: block;  padding-top: 20vh" class="content">
       <div class="container containergrey">
         <ul class="steps2">
