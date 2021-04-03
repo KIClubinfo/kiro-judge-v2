88916b4951be3b67228cc4ca9aba721359843aca
antoninparrot <antonin-parrot@hotmail.fr>
2021-04-04 01:53:49 +0200

    Update inscription.php

--- php/inscription.php
+++ php/inscription.php
@@ -341,8 +341,9 @@ if (isset($erreur)) {
 ?>
 
   <form action="" method="post" name="inscription">
-
+<?php popup($erreur, 6000, "error");?>
     <div id="equipe" style="display: block;  padding-top: 20vh" class="content">
+      <?php popup($erreur, 6000, "error");?>
       <div class="container containergrey">
         <?php popup($erreur, 6000, "error");?>
         <ul class="steps2">
@@ -351,11 +352,12 @@ if (isset($erreur)) {
           <li>Participant 2</li>
           <li>Participant 3</li>
         </ul>
-
+          <?php popup($erreur, 6000, "error");?>
         <label for="team-name">Nom d'équipe :</label>
         <input type="text" id="team-name" name="team-name" onkeydown="if (event.keyCode == 13)  document.getElementById('button-1').click()" maxlength="180" value="<?php if (isset($_POST['team-name'])) {
-                                                                                                                                                                      echo htmlspecialchars($_POST['team-name']);
-                                                                                                                                                                    } ?>" required> <br />
+          echo htmlspecialchars($_POST['team-name']);
+          } ?>" required> <br />
+          <?php popup($erreur, 6000, "error");?>
         <label for="team-hub">Choix du lieu:</label>
         <select name="team-hub" id="team-hub">
           <option value="1" <?php if (isset($_POST['team-hub'])) {
