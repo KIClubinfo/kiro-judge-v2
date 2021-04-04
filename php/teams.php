6ec00f6078c9df3b8d95cf7bfcff7af59edade15
antoninparrot <antonin-parrot@hotmail.fr>
2021-04-04 02:55:19 +0200

    Update teams.php

--- php/teams.php
+++ php/teams.php
@@ -192,7 +192,7 @@ if (isset($membre_3)) { //Si tout a bien marché on affiche tout
       <div class="cell"><?php echo htmlspecialchars($team_affiche->id) ?></div>
         <?php  } ?>
 
-        <div class="cell"><a href="teams.php?id_team=<?php echo htmlspecialchars($team_affiche->id) ?>><?php echo htmlspecialchars($team_affiche->nom); ?></a></div>
+        <div class="cell"><a href="teams.php?id_team=<?php echo htmlspecialchars($team_affiche->id) ?>"><?php echo htmlspecialchars($team_affiche->nom); ?></a></div>
         <div class="cell"><?php echo htmlspecialchars($team_affiche->classement); ?></div>
         <div class="cell"><?php echo htmlspecialchars($team_affiche->score); ?></div>
         <div class="cell"><?php if ($team_affiche->hub == 1) {
