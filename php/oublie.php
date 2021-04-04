278cf74fca841bf8bf22429a39f1628febd4926a
antoninparrot <antonin-parrot@hotmail.fr>
2021-04-04 03:14:54 +0200

    Update oublie.php

--- php/oublie.php
+++ php/oublie.php
@@ -68,30 +68,11 @@ if (!(isset($_SESSION['user']))) { //Si l'utilisateur n'est pas connecté
   }
 } else {
 
-
+  header('Location: /index.php?already_co');
+  exit();
 }
 
-if (isset($erreur)) {
-  //si on doit afficher le formulaire avec un message d'erreur
-  include("header.php");
-  popup($erreur, 6000, "error");
-?>
-  <div class="content" style="min-height: 70%;  margin-top: 20vh">
-    <div class="container containergrey">
-      <form action="" method="post">
-        <p style="text-align: center">Nous allons vous envoyer un nouveau mot de passe par mail. <br/></p>
-        <br/>
-        <label for="mail">Email :</label>
-        <input maxlength="255" type="email" name="email" value="<?php if (isset($_POST['email'])) {
-                                                                  echo htmlspecialchars($_POST['email']);
-                                                                } ?>" required><br />
-        <input type="submit" name="submit" value="Réinitialiser votre mot de passe">
-      </form>
-    </div>
-  </div>
 
-<?php
-}
 
 include("footer.php");
 ?>
