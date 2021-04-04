a45e6cc802067c3829567f6331d99b9f4a13d3bd
antoninparrot <antonin-parrot@hotmail.fr>
2021-04-04 02:08:05 +0200

    Update inscription.php

--- php/inscription.php
+++ php/inscription.php
@@ -134,10 +134,10 @@ if (!(isset($_SESSION['user']))) { //vérifie que l'utilisateur n'est pas connec
                                   include("send_mail.php");
 
 
-                                  send_mail($safe_email_1, htmlspecialchars($team_name), $safe_prenom_1, $safe_prenom_2, $safe_prenom_3, $safe_nom_2, $safe_nom_3, $email_1, $password1);
+                                  #send_mail($safe_email_1, htmlspecialchars($team_name), $safe_prenom_1, $safe_prenom_2, $safe_prenom_3, $safe_nom_2, $safe_nom_3, $email_1, $password1);
                                   #send_mail($safe_email_2, htmlspecialchars($team_name), $safe_prenom_2, $safe_prenom_3, $safe_prenom_1, $safe_nom_3, $safe_nom_1, $email_2, $password2);
                                   #send_mail($safe_email_3, htmlspecialchars($team_name), $safe_prenom_3, $safe_prenom_1, $safe_prenom_2, $safe_nom_1, $safe_nom_2, $email_3, $password3);
-                                  header('Location: /');
+                                  header('Location: /index.php');
                                   exit();
 
 
