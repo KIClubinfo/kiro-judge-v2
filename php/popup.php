1ed05db9fa86dffc6f2b36be85fc3e48150609e8
antoninparrot <antonin-parrot@hotmail.fr>
2021-04-04 02:05:04 +0200

    Update popup.php

--- php/popup.php
+++ php/popup.php
@@ -2,24 +2,24 @@
 // Permet de faire des jolies pop up pour les messages.
 
 function popup($message, $timer=6000, $icon='success') {
-    echo "<script>
+    echo '<script>
 const Toast = Swal.mixin({
   toast: true,
-  position: 'bottom-start',
+  position: "bottom-start",
   showConfirmButton: false,
   timer: $timer,
   timerProgressBar: true,
   didOpen: (toast) => {
-    toast.addEventListener('mouseenter', Swal.stopTimer)
-    toast.addEventListener('mouseleave', Swal.resumeTimer)
+    toast.addEventListener("mouseenter", Swal.stopTimer)
+    toast.addEventListener("mouseleave", Swal.resumeTimer)
   }
 })
 
 Toast.fire({
-  icon: '$icon',
-  title: '".htmlspecialchars($message)."'
+  icon: "'.$icon.'",
+  title: "'.$message.'"
 })
-</script>";
+</script>';
    }
 
 ?>
