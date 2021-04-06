6f1d95f19d90b7b2dd055a17c4f0d305f63b48cd
Pierre Glandon <pglandon78@gmail.com>
2021-04-06 14:21:55 +0200

    FDP Logo.

--- php/footer.php
+++ php/footer.php
@@ -10,6 +10,7 @@ $conn->close();
                     <div class="col-sm-2" style="display: block; margin: auto;"><a href="https://clubinfo.enpc.org" target="_blank"><img src="images/ki.png"></a></div>
                     <div class="col-sm-4" style="display: block; margin: auto;"><a href="https://cermics.enpc.fr"><img src="images/cermics.png"></a></div>
                     <div class="col-sm-2" style="display: block; margin: auto;"><a href="https://www.sncf.com/fr" target="_blank"><img src="images/sncf.png" height="80"></a></div>
+                    <div class="col-sm-2" style="display: block; margin: auto;"><a href="https://www.fondationdesponts.fr/" target="_blank"><img src="images/fdp.png" height="120"></a></div>
                 </div>
             </div>
         </div>
