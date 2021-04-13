c23b3f031a8efe903372d4d4a0bbf0aee22883ea
antoninparrot <antonin-parrot@hotmail.fr>
2021-04-13 22:55:15 +0200

    Update send_mail.php

--- php/send_mail.php
+++ php/send_mail.php
@@ -227,7 +227,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'kiro.enpc@gmail.com';                     //SMTP username
-    $mail->Password   = 'NXWd!DXjm5bWb^';                               //SMTP password
+    $mail->Password   = getenv("gmail_password");                              //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
