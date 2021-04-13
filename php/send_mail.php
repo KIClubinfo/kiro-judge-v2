0bb62bec38b09a69217f3f4d62996c4eed57a08c
Pierre Glandon <pglandon78@gmail.com>
2021-04-13 22:48:43 +0200

    fix env var access.

--- php/send_mail.php
+++ php/send_mail.php
@@ -123,7 +123,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'kiro.enpc@gmail.com';                     //SMTP username
-    $mail->Password   = $_ENV["gmail_password"];                               //SMTP password
+    $mail->Password   = getenv("gmail_password");                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
