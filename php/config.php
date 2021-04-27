4a084d9fc797c751c83188ef2548a5ec8ae7bfec
Pierre Glandon <pglandon78@gmail.com>
2021-04-27 18:14:11 +0200

    send_instance.php is working!

--- php/config.php
+++ php/config.php
@@ -4,8 +4,7 @@ include("popup.php");
 
 ##########NOT FOR PROD
 error_reporting(E_ALL);
-ini_set('display_errors','On');
-
+ini_set('display_errors', 'On');
 
 
 $db_password = $_ENV["mysql_password"];
@@ -28,115 +27,155 @@ $phpFileUploadErrors = array(
 $conn = new mysqli('db', 'kiro_user', $db_password, 'kiro');
 
 if ($conn->connect_error) {
-  die('Erreur lors de la connection à la base de donnée: ' . $conn->connect_error);
+    die('Erreur lors de la connection à la base de donnée: ' . $conn->connect_error);
 }
 
 
 class user
 {
-  public $id;
-  public $prenom;
-  public $nom;
-  public $id_team;
-  public $mail;
-  public $ecole;
-  public $tel;
-  public $admin;
-  public function __construct($id)
-  { // il faut vérfier en amont la donnée ici !
-    global $conn;
-    $this->id = $id; // id
-    if ($req = $conn->prepare("SELECT * FROM users WHERE id=?")) { // requête préparée
-      $req->bind_param("i", $id);
-      $req->execute();
-      $result = $req->get_result()->fetch_array(MYSQLI_ASSOC); // résulats de la requête
-      $req->close();
-      $this->prenom = htmlspecialchars($result['prenom']);
-      $this->admin =  $result['admin'];
-      $this->nom = htmlspecialchars($result['nom']);
-      $this->id_team = htmlspecialchars($result['id_team']);
-      $this->mail = htmlspecialchars($result['mail']);
-      $this->tel = htmlspecialchars($result['tel']);
-      $this->ecole = htmlspecialchars($result['ecole']);
-    } else {
-      echo "Erreur lors de la création de la classe user";
+    public $id;
+    public $prenom;
+    public $nom;
+    public $id_team;
+    public $mail;
+    public $ecole;
+    public $tel;
+    public $admin;
+
+    public function __construct($id)
+    { // il faut vérfier en amont la donnée ici !
+        global $conn;
+        $this->id = $id; // id
+        if ($req = $conn->prepare("SELECT * FROM users WHERE id=?")) { // requête préparée
+            $req->bind_param("i", $id);
+            $req->execute();
+            $result = $req->get_result()->fetch_array(MYSQLI_ASSOC); // résulats de la requête
+            $req->close();
+            $this->prenom = htmlspecialchars($result['prenom']);
+            $this->admin = $result['admin'];
+            $this->nom = htmlspecialchars($result['nom']);
+            $this->id_team = htmlspecialchars($result['id_team']);
+            $this->mail = htmlspecialchars($result['mail']);
+            $this->tel = htmlspecialchars($result['tel']);
+            $this->ecole = htmlspecialchars($result['ecole']);
+        } else {
+            echo "Erreur lors de la création de la classe user";
+        }
     }
-  }
 }
 
 class team
 {
-  public $id;
-  public $score;
-  public $classement;
-  public $hub;
-  public $nom;
-  public $valide;
-  public $numero_emplacement; // le numéro de sa table dans le hub
-  public $type_equipe;
-  public function __construct($id)
-  { // il faut vérfier en amont la donnée ici !
-    global $conn;
-    $this->id = intval($id); // id
-    if ($req = $conn->prepare("SELECT * FROM teams WHERE id=?")) { // requête préparée
-      $req->bind_param("i", $this->id);
-      var_dump($id);
-      $req->execute();
-      $result = $req->get_result()->fetch_array(MYSQLI_ASSOC); // résulats de la requête
-      $req->close();
-      $this->score = htmlspecialchars($result['score']);
-      $this->valide = $result['valide'];
-      $this->classement = htmlspecialchars($result['classement']);
-      $this->nom = htmlspecialchars($result['nom']);
-      $this->hub = htmlspecialchars($result['hub']);
-      $this->type_equipe = htmlspecialchars($result['type_equipe']);
-      $this->numero_emplacement = htmlspecialchars($result['numero_emplacement']);
-    } else {
-      echo "Erreur lors de la création de la classe team";
-    }
-  }
+    public $id;
+    public $score;
+    public $classement;
+    public $hub;
+    public $nom;
+    public $valide;
+    public $numero_emplacement; // le numéro de sa table dans le hub
+    public $type_equipe;
 
-
-  public function update_score($score)
-  { // Pour mettre à jour le score de l'équipe
-    global $conn;
-    if ($req = $conn->prepare("SELECT score FROM teams WHERE id=?")) { // requête préparée
-      $req->bind_param("i", $this->id);
-      $req->execute();
-      $result = $req->get_result()->fetch_array(MYSQLI_ASSOC); // résulats de la requête
-      $req->close();
-      $score_ancien = $result['score'];
-      if ($score > $score_ancien) { // si le score est meilleur
-        if ($req3 = $conn->prepare("UPDATE teams SET score =? WHERE id=?")) {
-          $req3->bind_param("si", $score, $this->id);
-          $req3->execute();
-          $req3->close();
-          $this->score = $score;
-          $contents = file_get_contents("http://node_12:8080/refresh"); // mise à jour des classements
-
+    public function __construct($id)
+    { // il faut vérfier en amont la donnée ici !
+        global $conn;
+        $this->id = intval($id); // id
+        if ($req = $conn->prepare("SELECT * FROM teams WHERE id=?")) { // requête préparée
+            $req->bind_param("i", $this->id);
+            $req->execute();
+            $result = $req->get_result()->fetch_array(MYSQLI_ASSOC); // résulats de la requête
+            $req->close();
+            $this->score = htmlspecialchars($result['score']);
+            $this->valide = $result['valide'];
+            $this->classement = htmlspecialchars($result['classement']);
+            $this->nom = htmlspecialchars($result['nom']);
+            $this->hub = htmlspecialchars($result['hub']);
+            $this->type_equipe = htmlspecialchars($result['type_equipe']);
+            $this->numero_emplacement = htmlspecialchars($result['numero_emplacement']);
         } else {
-          echo "Erreur lors de la mise à jour du score dans le update de la base de donnée.";
+            echo "Erreur lors de la création de la classe team";
         }
-      }
-    } else {
-      echo "Erreur lors de la mise à jour du score";
     }
-  }
+
+
+    public function update_score()
+    { // Pour mettre à jour le score de l'équipe
+        global $conn;
+        $score = 0;
+
+        $results = $conn->query("SELECT distinct (instance_id) , max(score) score from solutions where team_id = {$this->id} and score >= 0 group by instance_id;");
+        if (!empty($result)) {
+            while ($row = $results->fetch_assoc()) {
+                $score += $row["score"];
+            }
+        }
+
+
+        if ($req = $conn->prepare("SELECT score FROM teams WHERE id=?")) { // requête préparée
+            $req->bind_param("i", $this->id);
+            $req->execute();
+            $result = $req->get_result()->fetch_array(MYSQLI_ASSOC); // résulats de la requête
+            $req->close();
+            $score_ancien = $result['score'];
+            if ($score > $score_ancien) { // si le score est meilleur
+                if ($req3 = $conn->prepare("UPDATE teams SET score =? WHERE id=?")) {
+                    $req3->bind_param("si", $score, $this->id);
+                    $req3->execute();
+                    $req3->close();
+                    $this->score = $score;
+                    $contents = file_get_contents("http://node_12:8080/refresh"); // mise à jour des classements
+
+                } else {
+                    echo "Erreur lors de la mise à jour du score dans le update de la base de donnée.";
+                }
+            }
+        } else {
+            echo "Erreur lors de la mise à jour du score";
+        }
+    }
+}
+
+function create_solution($team_id, $user_id, $instance_id)
+{
+    global $conn;
+    $solution_id = -1;
+    if ($request = $conn->prepare("INSERT INTO solutions (instance_id, team_id, user_id) VALUES (?, ?, ?)")) {
+        $request->bind_param("iii", $instance_id, $team_id, $user_id);
+        $request->execute();
+        $solution_id = $request->insert_id;
+        $request->close();
+    }
+
+    return $solution_id;
+}
+
+function update_solution($solution_id, $score, $error)
+{
+    global $conn;
+    if ($request = $conn->prepare("UPDATE solutions SET errors = ?, score = ? WHERE solution_id = ?")) {
+        $request->bind_param("sii", $error, $score, $solution_id);
+        $request->execute();
+        $request->close();
+    }
+}
+
+function get_solution_filepath($instance_id, $team_id, $solution_id)
+{
+    return sprintf("/var/www/html/uploads/%i_%i_%i.json", $team_id, $instance_id, $solution_id);
 }
 
 function sanitize_string($str)
 {
-  global $conn;
-  $sanitize = mysqli_real_escape_string($conn, $str);
-  return $sanitize;
+    global $conn;
+    $sanitize = mysqli_real_escape_string($conn, $str);
+    return $sanitize;
 }
 
 function is_admin()
 { // renvoie true si l'user est un admin
-  if (isset($_SESSION['user'])) {
-    if ($_SESSION['user']->admin) {
-      return True;
+    if (isset($_SESSION['user'])) {
+        if ($_SESSION['user']->admin) {
+            return True;
+        }
     }
-  }
-  return False;
+    return False;
 }
--- php/send_instance.php
+++ php/send_instance.php
@@ -14,18 +14,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             // Update DB if score is valid
             // Send new score if OK
+            $team = new team($_SESSION["user"]->id_team);
+            $user = new user($_SESSION["user"]->id);
+
             foreach ($_FILES["solutions"]["error"] as $key => $error) {
                 if ($error == UPLOAD_ERR_OK) {
-                    $user = new user($_SESSION["user"]->id);
-                    var_dump($_SESSION["user"]);
-                    var_dump($user);
-                    $team = new team($_SESSION["user"]->id_team);
+
+                    $solution_id = create_solution($team->id, $user->id, $key);
                     $tmp_name = $_FILES["solutions"]["tmp_name"][$key];
-                    // basename() peut empêcher les attaques "filesystem traversal";
-                    // une autre validation/néttoyage du nom de fichier peux être appropriée
-                    $name = basename($_FILES["solutions"]["name"][$key]);
-                    move_uploaded_file($tmp_name, "/var/www/html/uploads/$name" . "_" . $team->nom);
-                    $command = "python3 /var/www/html/solution_checker/main.py -s %s -i %s";
+                    $file_path = get_solution_filepath($key, $team->id, $solution_id);
+
+
+                    move_uploaded_file($tmp_name, $file_path);
+                    $command = 'python3 /var/www/html/solution_checker/main.py -s "%s" -i "%s"';
 
                     $instances = [
                         "/var/www/html/solution_checker/instances/Asmall_fixed.json",
@@ -34,31 +35,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         "/var/www/html/solution_checker/instances/inst_PMP_fixed.json"
                     ];
 
-                    $command_format = sprintf($command, $tmp_name, $instances[$key]);
+
+                    $command_format = sprintf($command, $file_path, $instances[$key]);
                     $results = [];
-                    var_dump($command_format);
                     exec($command_format, $results);
 
                     $score = intval($results[0]);
-                    $is_better = false;
-
-                    if ($score >= $team->score) {
-                        // On enregistre le score dans la BDD.
-                        $is_better = true;
-                        global $conn;
-                        if ($request = $conn->prepare("UPDATE teams SET score = ? WHERE id = ?")) {
-                            $request.bind_param("ii", $score, $team->id);
-                            $request.execute();
-                            $request.close();
-                        }
-
+                    $errors_string = "";
+                    for ($i = 1; $i < sizeof($results); $i++) {
+                        $errors_string .= $results[$i] . PHP_EOL;
                     }
+
+                    update_solution($solution_id, $score, $errors_string);
+//                    $is_better = false;
+//
+//                    if ($score >= $team->score) {
+//                        // On enregistre le score dans la BDD.
+//                        $team->update_score($score);
+////                        $is_better = true;
+////                        global $conn;
+////                        if ($request = $conn->prepare("UPDATE teams SET score = ? WHERE id = ?")) {
+////                            $request.bind_param("ii", $score, $team->id);
+////                            $request.execute();
+////                            $request.close();
+////                        }
+//                        echo "New score: ";
+//                        echo $score;
+//
+//                    }
                 } else {
                     echo $phpFileUploadErrors[$error] ;
                     echo "<br />";
                     error_log("File not found.");
                 }
             }
+            $team->update_score();
             http_response_code(200);
         } else {
             http_response_code(400);
