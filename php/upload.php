2a488dfef1412d01f9dd8630e0262bd940e31448
davidnahmias2000@gmail.com <David>
2021-04-30 20:30:44 +0200

    page pour l'upload des instances

---
+++ php/upload.php
@@ -0,0 +1,19 @@
+<?php
+include("config.php");
+include("header.php");
+include("navbar.php");
+?>
+
+<div class=whitespace>
+  <div id="page" class="container">
+    <div class="title" style="margin-bottom: 1em" id="old">
+      <h2 style="font-size: 2.7em">Upload des Instances</h2>
+      <span class="byline">Vous pouvez déposer vos instances en cliquant sur le bouton ci-dessous :</span>
+    </div>
+    <ul class="actions">
+      <li><button class="button" onclick="self.location.href='drag/tamplate.html'">Upload</button></li>
+      <br>
+    </ul>
+  </div>
+</div>
+
