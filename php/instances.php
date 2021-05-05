20f1108961ea78fa23fcbbb013261ce651d31e5c
Pierre Glandon <pglandon78@gmail.com>
2021-05-05 23:25:21 +0200

    Truc qui aurait du etre fait il y a 10 ans.

--- php/instances.php
+++ php/instances.php
@@ -24,6 +24,7 @@ const WORST_SCORE = 58831240;
 
 function display_errors_button($errors) {
     $errors_format = str_replace(PHP_EOL, "<br/>", $errors);
+    $errors_format = str_replace("'", "\\'", $errors_format);
     ?>
     <button class="error-button" onclick="let x = <?php echo "'" . $errors_format . "'"; ?>;popUp(x)">Errors</button>
 <?php
