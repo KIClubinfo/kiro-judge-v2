8638e612394097f4b4f7346c96d6f1a4bb6f9507
Thomas <thomasbuiron78@gmail.com>
2021-11-25 16:56:57 +0100

    You can come back tiny it was not your fault

--- php/instances.php
+++ php/instances.php
@@ -1,23 +1,26 @@
 <?php
 const INSTANCE_NAMES = [
+    "KIRO-tiny",
     "KIRO-small",
     "KIRO-medium",
     "KIRO-large"
 ];
 
 const INSTANCE_FILES = [
+    "/var/www/html/solution_checker/instances/KIRO-tiny.json",
     "/var/www/html/solution_checker/instances/KIRO-small.json",
     "/var/www/html/solution_checker/instances/KIRO-medium.json",
     "/var/www/html/solution_checker/instances/KIRO-large.json"
 ];
 
 const INSTANCE_SCORES = [
+    5000000,
     1000000000,
     5000000000,
     300000000000
 ];
 
-const WORST_SCORE = 306000000000;
+const WORST_SCORE = 306005000000;
 
 function display_errors_button($errors) {
     $errors_format = str_replace(PHP_EOL, "<br/>", $errors);
