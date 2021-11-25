b7a29f7ab837e4832fc2abcf709ef6654c7c0371
Thomas <thomasbuiron78@gmail.com>
2021-11-25 18:26:00 +0100

    En fait non on fait comme ça

--- php/upload.php
+++ php/upload.php
@@ -64,9 +64,11 @@ include("navbar.php");
             $command_format = sprintf($command, $file_path, INSTANCE_FILES[$key]);
               $results = [];
             exec($command_format, $results);
-            $score = intval($results[0]);
-            if($score!=-1){
-              $score = $score/10000;
+            if($results[0]!=-1){
+              $score = intval($results[0]/10000);
+            }
+            else{
+              $score = -1;
             }
             $errors_string = "";
             for ($i = 1; $i < sizeof($results); $i++) {
