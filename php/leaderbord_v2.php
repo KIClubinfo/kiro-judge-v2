77e0a3f4e529dcd74b04dfc907c591a5ebf48919
Dadou15 <davidnahmias2000@gmail.com>
2021-11-25 15:10:07 +0100

    ld-v2

---
+++ php/leaderbord_v2.php
@@ -0,0 +1,47 @@
+<?php
+include("config.php");
+include("header.php");
+include("navbar.php");
+
+if ($req2 = $conn->prepare("SELECT id FROM teams")) { //toutes les id des teams
+    $req2->execute();
+    $result_ids = $req2->get_result()->fetch_all(MYSQLI_ASSOC); //resulats de la requête
+
+    $req2->close();
+}
+else{
+    $erreur3 = "Erreur lors de la connexion à la base de données.";
+    die();
+}
+?>
+<header class="masthead min-vh-80">
+      <div class="container">
+          <div class="table-responsive">
+            <table class="box-tableau table table-hover text-white">
+                <thead>
+                  <tr class="table-dark">
+                    <th scope="col">Nom d'équipe</th>
+                    <th scope="col">Score</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  foreach($result_ids as $id_team){
+                  $id_team = $id_team["id"];
+                  $team_affiche = new team($id_team);
+                  ?>
+                  <tr>
+                    <tr scope="row"><?php echo htmlspecialchars($team_affiche->nom); ?></tr>
+                    <td><?php echo htmlspecialchars($team_affiche->public_score); ?></td>
+                  </tr>
+                  <?php
+                  }
+                  }?>
+                </tbody>
+            </table>
+          </div>
+      </div>
+  </header>
+  <?php
+  include("footer.php");
+  ?>
