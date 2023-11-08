2695c4bd5f9c81d27c3022e5a2dca2040e504d30
adcrry <ducorroyalexandre@gmail.com>
2023-11-08 10:07:41 +0100

    changed registration limit date

--- php/config.php
+++ php/config.php
@@ -10,7 +10,7 @@ ini_set('display_errors', 'Off');
 $db_password = $_ENV["mysql_password"];
 
 setlocale(LC_TIME, ['fr', 'fra', 'fr_FR']);
-$date_limite_inscription = date_create('2023-11-08');
+$date_limite_inscription = date_create('2023-11-09');
 $aujourdhui = new DateTime("now");
 $dateconcours = new DateTime('2023-11-09 14:00:00');
 $datefinconcours = new DateTime('2023-11-09 20:00:00');
